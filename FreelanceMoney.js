40b17bed9bb776b45e49eb707f29b08d58abb189
Angélique Robert <angel97410.ar@gmail.com>
2022-05-30 16:31:09 +0200

    Javascript op

--- FreelanceMoney.js
+++ FreelanceMoney.js
@@ -1,3 +1,16 @@
 function CalculGain(){
   let myForm = document.getElementById("formCalculGain")
+  let th = Number(document.getElementById('TauxHoraire').value)
+  let tjm = Number(document.getElementById("TauxJournalier").value)
+  let extras = Number(document.getElementById("TauxExtras").value)
+  let QteTH = Number(document.getElementById("QteTH").value)
+  let QteTJM = Number(document.getElementById("QteTJM").value)
+  let QteExtras = Number(document.getElementById("QteExtras").value)
+  let Charges = Number(document.getElementById("Charges").value)
+
+  let brut = (th * QteTH) + (tjm * QteTJM) + (extras * QteExtras)
+  let net = brut - (Charges/100)
+  document.getElementById("resultB").innerHTML = "Le tarif BRUT : " + brut + "€";
+  document.getElementById("resultN").innerHTML = "Le tarif NET : " + net + "€";
+  return brut, net;
 }
