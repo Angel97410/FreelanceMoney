43ea7d88d74d3af99429313ca78406d28c57ce56
Angélique Robert <angel97410.ar@gmail.com>
2022-05-31 13:56:13 +0200

    input disabled

--- FreelanceMoney.js
+++ FreelanceMoney.js
@@ -1,4 +1,4 @@
-function CalculGain(){
+function CalculGain() {
   let myForm = document.getElementById("formCalculGain")
   let th = Number(document.getElementById('TauxHoraire').value)
   let tjm = Number(document.getElementById("TauxJournalier").value)
@@ -9,8 +9,33 @@ function CalculGain(){
   let Charges = Number(document.getElementById("Charges").value)
 
   let brut = (th * QteTH) + (tjm * QteTJM) + (extras * QteExtras)
-  let net = brut - (Charges/100)
+  let net = brut - (Charges / 100)
   document.getElementById("resultB").innerHTML = "Le tarif BRUT : " + brut + "€";
   document.getElementById("resultN").innerHTML = "Le tarif NET : " + net + "€";
   return brut, net;
+}
+document.getElementById("TauxHoraire").onkeyup = function() {disableInput()}
+document.getElementById("TauxJournalier").onkeyup = function() {disableInput()}
+document.getElementById("QteTH").onkeyup = function() {disableInput()}
+document.getElementById("QteTJM").onkeyup = function() {disableInput()}
+
+function disableInput() {
+  // réferencement button
+ let th = Number(document.getElementById('TauxHoraire').value)
+  let tjm = Number(document.getElementById("TauxJournalier").value)
+  let QteTH = Number(document.getElementById("QteTH").value)
+  let QteTJM = Number(document.getElementById("QteTJM").value)
+  console.log(th)
+  if (th || QteTH != 0){
+    document.getElementById("TauxJournalier").disabled = true;
+    document.getElementById("QteTJM").disabled = true;
+  }else{
+    document.getElementById("TauxJournalier").disabled = false;
+    document.getElementById("QteTJM").disabled = false;
+  }
+  if (tjm || QteTJM != 0){
+    document.getElementById("TauxHoraire").disabled = true;
+    document.getElementById("QteTH").disabled = true;
+  }
+  
 }
